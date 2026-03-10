4cf6b5e2c57106304e5470cf79657f6a5d8cdd0c
terawww <negrc529@gmail.com>
2026-03-10 14:35:05 +0100

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "06.07.2006"; // Дата рождения
+var fullname = "Сачук Олександр Андрійович " // ФИО
+var name = "Олександр" // Имя
+
+// Паспорт
+var passport_id = "397080898" // Номер паспорта
+
+// КПП
+var kpp_id = "0123456789" // ИНН
+
+// Права
+var rights_valid_until = "01.01.2026" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "11.11.2026" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
